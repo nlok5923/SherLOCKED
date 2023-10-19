9b44eb14d6928bc23537ce3de9d7c017167845db
Madhur Gupta <madhurgupta107@gmail.com>
2023-10-19 17:11:15 +0530

    Init sdk dir

---
+++ sdk/src/FHETransactionBuilder.js
@@ -0,0 +1,13 @@
+class FHETransactionBuilder {
+    constructor(address) {
+        this.address = address;
+    }
+
+    getBalance() {
+        // call the API to get the decrypted balance of this.address
+    }
+
+    sendTransaction(to, amount) {
+        // call the API to send a transaction
+    }
+}
