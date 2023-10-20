b838a7c7c7f79ab8842cc6dc24a6a7404097918c
Madhur Gupta <madhurgupta107@gmail.com>
2023-10-20 22:28:59 +0530

    Integerate balance with frontend

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,16 +1,66 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
+
+import { useWallets, useConnectWallet } from "@web3-onboard/react";
+
 import { Button } from "./ui/button";
 
 const Balance = () => {
+  const trxBuilder = useRef(null);
+
+  const [{ wallet }] = useConnectWallet();
+  const connectedWallets = useWallets();
+
   const [isBalanceEncrypted, setIsBalanceEncrypted] = useState(true);
   const [balance, setBalance] = useState("Loading...");
+
+  useEffect(() => {
+    (async () => {
+      if (!connectedWallets[0]?.accounts[0]?.address) {
+        return;
+      }
+
+      // create an ethers provider
+      let provider = new ethers.BrowserProvider(wallet.provider, "any");
+
+      if (!provider) {
+        alert("Please connect a wallet first.");
+        return;
+      }
+
+      trxBuilder.current = new FHETransactionBuilder(
+        connectedWallets[0].accounts[0].address
+      );
+      setBalance(await trxBuilder.current.getEncryptedBalance({ provider }));
+    })();
+  }, [connectedWallets]);
+
+  const getDecryptedBalance = async () => {
+    if (!connectedWallets[0]?.accounts[0]?.address) {
+      return;
+    }
+
+    let provider = new ethers.BrowserProvider(wallet.provider, "any");
+
+    if (!provider) {
+      alert("Please connect a wallet first.");
+      return;
+    }
+
+    setIsBalanceEncrypted(false);
+    setBalance(
+      await trxBuilder.current.getDecryptedBalance({
+        signer: await provider.getSigner(),
+      })
+    );
+  };
+
   return (
     <div className="flex flex-col justify-center items-center border-2 gap-y-3 border-purple-100 p-4">
       <div className="flex gap-x-2 w-fit">
         <h2 className="w-fit">Balance:</h2>
         <p className="w-fit">{balance}</p>
       </div>
-      <Button className="w-fit">
+      <Button className="w-fit" onClick={getDecryptedBalance}>
         {isBalanceEncrypted ? "Decrypt" : "Hide"}
       </Button>
     </div>
--- sdk/src/FHETransactionBuilder.js
+++ sdk/src/FHETransactionBuilder.js
@@ -5,25 +5,42 @@ const eERC20_ABI = require("./constants/abi/eERC20.json");
 const eERC20_ADDRESS = "0x7cC82f365A448918Ea79e4DcA62ACeA24B0C3894";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001'
-})
+  baseURL: "http://localhost:3001",
+});
 
 class FHETransactionBuilder {
   constructor(address) {
     this.address = address;
   }
 
-  async getBalance({ signer }) {
-    const eERC20Contract = new ethers.Contract(
-      eERC20_ADDRESS,
-      eERC20_ABI,
-      signer
-    );
-
+  async getEncryptedBalance({ provider }) {
     // call balanceOf method to get encrypted balance of this.address
-    const encryptedBalance = parseInt(
-      await eERC20Contract.balanceOf(this.address)
-    );
+    const sendTransaction = {
+      to: eERC20_ADDRESS,
+      data: new ethers.utils.Interface(eERC20_ABI.abi).encodeFunctionData(
+        "balanceOf",
+        [this.address]
+      ),
+      value: "0",
+      gasLimit: "100000",
+    };
+
+    console.log(" this is the amount ", sendTransaction);
+
+    // call the transfer function with to, cipherAmount
+    const txn = await provider.sendTransaction(sendTransaction);
+
+    console.log("this is txn", txn);
+
+    const encryptedBalance = parseInt(trx.data);
+
+    return encryptedBalance;
+  }
+
+  async getDecryptedBalance({ signer }) {
+    const encryptedBalance = await this.getEncryptedBalance({
+      provider: signer,
+    });
 
     // sign a message to prove owner of this.address
     const message = "FHE Transaction Builder for the win!";
@@ -47,42 +64,33 @@ class FHETransactionBuilder {
       plainTextAmount: amount,
     });
 
-    console.log(' this is rs p', resp)
+    console.log(" this is rs p", resp);
 
     const { encryptedAmount } = resp.data;
 
-    console.log('this is amount ', amount);
+    console.log("this is amount ", amount);
 
-    console.log('encrpted amt ', encryptedAmount);
-    console.log('paruint  amt ', ethers.utils.parseUnits(encryptedAmount, 0));
+    console.log("encrpted amt ", encryptedAmount);
+    console.log("paruint  amt ", ethers.utils.parseUnits(encryptedAmount, 0));
 
-
-    console.log('this is signer ', signer);
-
-    // const eERC20Contract = new ethers.Contract(
-    //   eERC20_ADDRESS,
-    //   eERC20_ABI.abi,
-    //   signer
-    // );
+    console.log("this is signer ", signer);
 
     const sendTransaction = {
       to: eERC20_ADDRESS,
-      data: new ethers.utils.Interface(eERC20_ABI.abi).encodeFunctionData('transfer', [
-        to,
-        ethers.utils.parseUnits(encryptedAmount, 0)
-      ]),
+      data: new ethers.utils.Interface(eERC20_ABI.abi).encodeFunctionData(
+        "transfer",
+        [to, ethers.utils.parseUnits(encryptedAmount, 0)]
+      ),
       value: "0",
-      gasLimit: '100000'
+      gasLimit: "100000",
     };
 
-    console.log(' this is the amount ', sendTransaction);
-
-    const txn = await signer.sendTransaction(sendTransaction);
-
-    console.log('this is txn', txn);
+    console.log(" this is the amount ", sendTransaction);
 
     // call the transfer function with to, cipherAmount
-    // await eERC20Contract.transfer(to, encryptedAmount);
+    const txn = await signer.sendTransaction(sendTransaction);
+
+    console.log("this is txn", txn);
 
     return true;
   }
